879e1dba8db06bb3e092d69991cc0bb7a608af71
Eric Shelbogashev <96541869+EricShelbogashev@users.noreply.github.com>
2023-12-22 14:29:12 +0700

    init

---
+++ src/main/java/ru/nsu/ooad/aemsdemo/AemsDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.ooad.aemsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AemsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AemsDemoApplication.class, args);
+    }
+
+}
